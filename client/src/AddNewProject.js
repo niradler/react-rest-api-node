d5a94a6535ec2dc828d777f04f445bd994b08ccb
Nir <niradler55@gmail.com>
2017-10-24 20:45:48 +0300

    add

--- client/src/AddNewProject.js
+++ client/src/AddNewProject.js
@@ -1,4 +1,6 @@
-import React, {Component} from 'react';
+import React, {
+    Component
+} from 'react';
 class AddNewProject extends Component {
     constructor(props) {
         super()
@@ -7,7 +9,7 @@ class AddNewProject extends Component {
             authorLink: "http://renanlopes.com",
             demoLink: "https://typescript-angular4.herokuapp.com/",
             description: "Desenvolvimento Skills em TypeScript, Orientação a Objeto, Angular 2 / 4 e WebPa" +
-                    "ck",
+                "ck",
             githubLink: "https://github.com/renanlopescoder/typescript-angular4",
             project: "Project Manager",
             status: "Disponível",
@@ -15,189 +17,241 @@ class AddNewProject extends Component {
             technologies: "Angular 2/4, TypeScript e Webpack",
             projects: []
         };
+        this.getProjects = this.getProjects.bind(this);
+        this.addProjects = this.addProjects.bind(this);
+        this.getProjects();
     }
-    handleSubmit(event) {
-        event.preventDefault();
-        this
+    getProjects() {
+        let self=this;
+        fetch('http://localhost:3000/list/projects').then((resp) => resp.json())
+            .then(function (data) {
+                self.setState({
+                    projects: data
+                });
+            });
+    }
+    addProjects() {
+       let self = this;
+        self
             .state
             .projects
             .push({
-                author: this.state.author,
-                authorLink: this.state.authorLink,
-                demoLink: this.state.demoLink,
-                description: this.state.description,
-                githubLink: this.state.githubLink,
-                project: this.state.project,
-                status: this.state.status,
-                technologies: this.state.technologies
+                author: self.state.author,
+                authorLink: self.state.authorLink,
+                demoLink: self.state.demoLink,
+                description: self.state.description,
+                githubLink: self.state.githubLink,
+                project: self.state.project,
+                status: self.state.status,
+                technologies: self.state.technologies,
+                _id: self.state.projects.length
             })
-        this.setState({projects: this.state.projects})
+        self.setState({
+            projects: self.state.projects
+        })
+    }
+    putProjects() {
 
     }
-    handleChange(event) {
+    delProjects(id) {
+        this.state.projects=this.state.projects.filter((item) => {
+                return item._id !== id
+            })
+           
+        this.setState({
+            projects: this.state.projects
+        });
+    }
 
+    handleSubmit(event) {
+        event.preventDefault();
+        this.addProjects();
+    }
+
+    handleChange(event) {
         let obj = {};
         obj[event.target.name] = event.target.value;
         this.setState(obj);
     }
 
     render() {
-        var listItems = this.state.projects.map(function (item) {
-            let i=0;
-                return (
-                    <div className="box">
-                    <article className="media">
-                        <div className="content">
-                         
-                              <ul>
-                          <li key={i++}>
-                        <div>{item.author}</div>
-                        <div>{item.authorLink}</div>
-                        <div>{item.demoLink}</div>
-                        <div>{item.description}</div>
-                        <div>{item.project}</div>
-                        <div>{item.technologies}</div>
-                    </li>
-                    </ul>
-                         
-                      </div>
-                    </article>
-                  </div>
-                  
-                );
-            });
 
-        return (
-            <div className="container">
-
-                <form
-                    onSubmit={this.handleSubmit}
-                    onSubmit={this.handleSubmit.bind(this)}>
-                   
-                    <div className="field  ">
-                        <label className="label">author</label>
-                        <div className="control">
-                        <input
-                        className="input"
-                        type="text"
-                        name="author"
-                        value={this.state.author}
-                        onChange={this
-                        .handleChange
-                        .bind(this)}/>
-                        </div>
-                    </div>
+        var listItems = this
+            .state
+            .projects
+            .map( (item)=> {
                     
-                    <div className="field ">
-                        <label className="label">authorLink</label>
-                        <div className="control">
-                        <input
-                        className="input"
-                        type="text"
-                        name="authorLink"
-                        value={this.state.authorLink}
-                        onChange={this
-                        .handleChange
-                        .bind(this)}/>
-                        </div>
-                    </div>
+                    return ( < div className = "box" > < article className = "media" > < div className = "content" > < ul > < li key = {
+                            item._id
+                        } >
+< button type = "button" className = "button" onClick={this.delProjects.bind(this, item._id)} > remove < /button >
+                        <
+                        div > {
+                            item.author
+                        } < /div> <
+                        div > {
+                            item.authorLink
+                        } < /div > < div > {
+                        item.demoLink
+                    } < /div> <
+                    div > {
+                        item.description
+                    } < /div > < div > {
+                    item.project
+                } < /div> <
+                div > {
+                    item.technologies
+                } < /div > < /
+                li > <
+                /ul > < /div > < /article > < /div >
 
-                    <div className="field">
-                        <label className="label">demoLink</label>
-                        <div className="control">
-                        <input
-                        className="input"
-                        type="text"
-                        name="demoLink"
-                        value={this.state.demoLink}
-                        onChange={this
-                        .handleChange
-                        .bind(this)}/>
-                        </div>
-                    </div>
+            );
+    });
 
-                    <div className="field">
-                        <label className="label">description</label>
-                        <div className="control">
-                        <input
-                        className="input"
-                        type="text"
-                        name="description"
-                        value={this.state.description}
-                        onChange={this
-                        .handleChange
-                        .bind(this)}/>
-                        </div>
-                    </div>
+return ( <
+    div className = "container" >
 
+    <
+    form 
+   
+    onSubmit = {
+        this
+        .handleSubmit
+        .bind(this)
+    } >
 
-                    <div className="field">
-                        <label className="label">githubLink</label>
-                        <div className="control">
-                        <input
-                        className="input"
-                        type="text"
-                        name="githubLink"
-                        value={this.state.githubLink}
-                        onChange={this
-                        .handleChange
-                        .bind(this)}/>
-                        </div>
-                    </div>
+    <
+    div className = "field  " >
+    <
+    label className = "label" > author < /label > < div className = "control" > < input className = "input" type = "text" name = "author" value = {
+    this.state.author
+}
+onChange = {
+    this
+    .handleChange
+    .bind(this)
+}
+/> < /
+div > <
+    /div > < div className = "field " > < label className = "label" > authorLink < /label > <
+    div className = "control" >
+    <
+    input className = "input"
+type = "text"
+name = "authorLink"
+value = {
+    this.state.authorLink
+}
+onChange = {
+    this
+    .handleChange
+    .bind(this)
+}
+/ > < /
+div > <
+    /div > < div className = "field" > < label className = "label" > demoLink < /label > <
+    div className = "control" >
+    <
+    input className = "input"
+type = "text"
+name = "demoLink"
+value = {
+    this.state.demoLink
+}
+onChange = {
+    this
+    .handleChange
+    .bind(this)
+}
+/ > < /
+div > <
+    /div > < div className = "field" > < label className = "label" > description < /label > <
+    div className = "control" >
+    <
+    input className = "input"
+type = "text"
+name = "description"
+value = {
+    this.state.description
+}
+onChange = {
+    this
+    .handleChange
+    .bind(this)
+}
+/ > < /
+div > <
+    /div > < div className = "field" > < label className = "label" > githubLink < /label > <
+    div className = "control" >
+    <
+    input className = "input"
+type = "text"
+name = "githubLink"
+value = {
+    this.state.githubLink
+}
+onChange = {
+    this
+    .handleChange
+    .bind(this)
+}
+/ > < /
+div > <
+    /div > < div className = "field" > < label className = "label" > project < /label > <
+    div className = "control" >
+    <
+    input className = "input"
+type = "text"
+name = "project"
+value = {
+    this.state.project
+}
+onChange = {
+    this
+    .handleChange
+    .bind(this)
+}
+/ > < /
+div > <
+    /div > < div className = "field" > < label className = "label" > technologies < /label > <
+    div className = "control" >
+    <
+    input className = "input"
+type = "text"
+name = "technologies"
+value = {
+    this.state.technologies
+}
+onChange = {
+    this
+    .handleChange
+    .bind(this)
+}
+/ > < /
+div > <
+    /div > < div className = "field" > < label className = "label" > status < /label > <
+    div className = "control" >
+    <
+    input className = "input"
+type = "text"
+name = "status"
+value = {
+    this.state.status
+}
+onChange = {
+    this
+    .handleChange
+    .bind(this)
+}
+/ > < /
+div > <
+    /div > < button type = "submit" className = "button" > submit < /button >
 
-
-                    <div className="field">
-                        <label className="label">project</label>
-                        <div className="control">
-                        <input
-                        className="input"
-                        type="text"
-                        name="project"
-                        value={this.state.project}
-                        onChange={this
-                        .handleChange
-                        .bind(this)}/>
-                        </div>
-                    </div>
-
-                    <div className="field">
-                        <label className="label">technologies</label>
-                        <div className="control">
-                        <input
-                        className="input"
-                        type="text"
-                        name="technologies"
-                        value={this.state.technologies}
-                        onChange={this
-                        .handleChange
-                        .bind(this)}/>
-                        </div>
-                    </div>
-
-
-                    <div className="field">
-                        <label className="label">status</label>
-                        <div className="control">
-                        <input
-                        className="input"
-                        type="text"
-                        name="status"
-                        value={this.state.status}
-                        onChange={this
-                        .handleChange
-                        .bind(this)}/>
-                        </div>
-                    </div>
-    
-                    <button type="submit" className="button">submit</button>
-                   
-                </form>
-                
-                {/* <ul> */}
-                    {listItems}
-                {/* </ul> */}
-            </div>
-        );
-    }
+    <
+    /form > {
+listItems
+} < /div>
+);
+}
 }
 export default AddNewProject;
