1e957a4ec46197eefc59724ffe50837726b680da
Nir <niradler55@gmail.com>
2017-11-03 17:09:20 +0200

    jest

--- client/src/components/Project.test.js
+++ client/src/components/Project.test.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Projects from './Projects';
-
+import {Provider} from 'mobx-react';
+import ProjectsModel from "../store/ProjectsModel";
+const store = new ProjectsModel();
+const Root = (
+  <Provider store={store}>
+  <Projects />
+  </Provider>
+)
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<Projects />, div);
+  ReactDOM.render(Root, div);
 });
